a1031088bba35262e1465dd8db1d1c345cddd0b7
Chimoha31 <mokochii1108@gmail.com>
2022-03-14 23:57:05 -0700

    opacity

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const off = document.querySelector('.off');
 
 on.addEventListener('click', function() {
   audio.play();
+  on.style.opacity = '1';
+  off.style.opacity = '0.5';
 })
 off.addEventListener('click', function() {
   audio.pause();
+  off.style.opacity = '1';
+  on.style.opacity = '0.5';
 })
