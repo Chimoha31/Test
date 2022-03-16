516c9527bac1d0963481d59a010f66e14d307770
Chimoha31 <mokochii1108@gmail.com>
2022-03-16 13:44:51 -0700

    init

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ off.addEventListener('click', function() {
   on.style.opacity = '0.5';
 })
 
-
 const a = document.querySelectorAll("a");
 VanillaTilt.init(document.querySelector(".inner"), {
   max: 5,
@@ -25,31 +24,25 @@ VanillaTilt.init(document.querySelector(".inner"), {
 
 
 $(function(){
-  //カーソル要素の指定
-  var cursor=$("#cursor");
-  //ちょっと遅れてついてくるストーカー要素の指定  
+  var cursor=$("#cursor"); 
   var stalker=$("#stalker");
   
-  //mousemoveイベントでカーソル要素を移動させる
   $(document).on("mousemove",function(e){
-    //カーソルの座標位置を取得
     var x=e.clientX;
     var y=e.clientY;
-    //カーソル要素のcssを書き換える用
     cursor.css({
       "opacity":"1",
       "top":y+"px",
       "left":x+"px"
     });
-    //ストーカー要素のcssを書き換える用    
+   
     setTimeout(function(){
       stalker.css({
         "opacity":"1",
         "top":y+"px",
         "left":x+"px"
       });
-    },140);//カーソルより遅れる時間を指定
-    
+    },140);
   });
 });
 
