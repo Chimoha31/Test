4cd7a4b6685203499a4e9f9664ed09c92e290f7d
Chimoha31 <mokochii1108@gmail.com>
2022-03-17 17:09:00 -0700

    Fixed bug

---
+++ about_me.js
@@ -0,0 +1,10 @@
+'use strict';
+const page = document.querySelector('.page');
+const menuToggle = document.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+  page.classList.toggle('rotate');
+});
+
+
